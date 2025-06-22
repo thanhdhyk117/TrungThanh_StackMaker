5c878c1a5f47be6c04135ec86e12f454c2d4db4a
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-06-22 16:14:24 +0700

    + Add script make map

---
+++ Assets/00_Thanhf/Scripts/Map/CreateMap.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour
+{
+    [SerializeField] GameObject _brickPrefab;
+    [SerializeField] Transform startInstance;
+
+    [SerializeField] int _horizontalValue, _verticalValue;
+
+    [ContextMenu("Make map")]
+    private void MakeMap()
+    {
+        CreateMapByMatrix(_horizontalValue, _verticalValue);
+    }
+
+
+    private void CreateMapByMatrix(int _x, int _y)
+    {
+        if (_x <= 0 || _y <= 0) return;
+
+        float prefabScaleX = _brickPrefab.transform.localScale.x;
+        float prefabScaleY = _brickPrefab.transform.localScale.y;
+
+        for (int i = 0; i < _x; i++)
+        {
+            for (int j = 0; j < _y; j++)
+            {
+                Vector3 _newObjPos = startInstance.position + new Vector3(prefabScaleX * i * transform.localScale.x, 0, prefabScaleY * j * transform.localScale.z);
+
+                GameObject _newObj = Instantiate(_brickPrefab, _newObjPos, Quaternion.Euler(new Vector3(-90, 0, -180)), transform);
+                _newObj.SetActive(true);
+            }
+        }
+    }
+}
