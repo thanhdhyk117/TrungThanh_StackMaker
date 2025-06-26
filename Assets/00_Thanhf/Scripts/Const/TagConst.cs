5d585ae646e88ce00c291626b1bfd3c72deb23e4
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-06-26 14:23:20 +0700

    ~ Update tag value

--- Assets/00_Thanhf/Scripts/Const/TagConst.cs
+++ Assets/00_Thanhf/Scripts/Const/TagConst.cs
@@ -5,4 +5,7 @@ public class TagConst
     public const string TAG_GROUND = "Wall";
     public const string TAG_BRIDGE = "Bridge";
     public const string TAG_WIN_AREA = "WinArea";
+    public const string TAG_START_AREA = "StartPoint";
+    public const string TAG_FINISH_AREA = "FinishPoint";
+
 }
